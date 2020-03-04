b276754b0f2da0de2e0ce01072f7a0bb54f0b804
pashkovka115 <pariss8@mail.ru>
2020-03-04 14:34:52 +0300

    add more markers

--- 3.5_marks/test_fixture8.py
+++ 3.5_marks/test_fixture8.py
@@ -15,13 +15,14 @@ def browser():
 
 class TestMainPage1():
     # pytestmark = [pytest.mark.webtest, pytest.mark.slowtest]
-    
+
     @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector("#login_link")
 
     @pytest.mark.regression
+    @pytest.mark.win10
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
