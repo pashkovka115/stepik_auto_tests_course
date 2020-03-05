8b5475249cae5783db94d2ada569cee9d7c3d4f1
pashkovka115 <pariss8@mail.ru>
2020-03-05 08:36:36 +0300

    add plugin pytest-rerunfailures

---
+++ 3.6_params/test_rerun.py
@@ -0,0 +1,13 @@
+# command
+# pytest github_repo_home-work/3.6_params/test_rerun.py -v --tb=line --reruns 1
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#magic_link")
+
